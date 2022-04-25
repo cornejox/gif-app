3211801053524b001d67dc1b97cc19a16d3a66fa
Juan Manuel Callejas Cornejo <jcallejascornejo@gmail.com>
2022-04-24 21:48:56 -0500

    add history

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -12,7 +12,10 @@ export class BusquedaComponent {
     @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
     buscar(){
+
         const valor = this.txtBuscar.nativeElement.value;
+
+        if(valor.trim().length === 0){return;}
         this.gifsService.buscarGifs( valor );
         this.txtBuscar.nativeElement.value = '';
     }
