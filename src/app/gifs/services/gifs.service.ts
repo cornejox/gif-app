b705e10e5f7a380f7dc8125608aa28845a187bb8
Juan Manuel Callejas Cornejo <jcallejascornejo@gmail.com>
2022-04-24 22:35:34 -0500

    add api

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,16 +1,22 @@
 import { query } from '@angular/animations';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+    private apiKey: string = 'bRsqICV9sJhqwtOA1sYPz9O3cD1dTif3';
     public _historial: string[] = [];
+
+    public resultados: any[] = [];
 
     get historial(){
         return [...this._historial];
     }
+
+    constructor( private http: HttpClient){}
 
     buscarGifs(query: string = ''){
 
@@ -18,9 +24,16 @@ export class GifsService {
         if(!this._historial.includes(query)){
             this._historial.unshift(query);
             this._historial = this._historial.splice(0,10);
-        };        
+        };
+
         console.log(this._historial);
-        }
+
+        this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=bRsqICV9sJhqwtOA1sYPz9O3cD1dTif3&q=${ query } z&limit=10`)
+            .subscribe(( resp:any ) => {
+                console.log(resp.data);
+                this.resultados = resp.data;
+            });
+    }
 
     
 
