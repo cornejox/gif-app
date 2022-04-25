0d994696477f50d954e015ac190f22759579395c
cornejox <jcallejascornejo@gmail.com>
2022-04-25 11:25:24 -0500

    change limit

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -38,7 +38,7 @@ export class GifsService {
 
         const params = new HttpParams()
             .set('api_key', this.apiKey)
-            .set('limit', '10')
+            .set('limit', '20')
             .set('q', query);
         
         this.http.get<SearchGifsResponse>(`${this.servicioURL}/search`, {params})
